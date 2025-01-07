1406786c4d7c38309c57f454090bd2c4274f43a6
Sandy <sandyrajalamsyah@gmail.com>
2025-01-07 16:04:48 +0700

    chore: remove unused util function

--- src/main/java/com/imura/VizMem/Utils.java
+++ src/main/java/com/imura/VizMem/Utils.java
@@ -17,17 +17,6 @@ public class Utils {
         }
     }
 
-    public static void setTimeout(Runnable runnable, int delay) {
-        new Thread(() -> {
-            try {
-                Thread.sleep(delay);
-                runnable.run();
-            } catch (Exception e) {
-                System.err.println(e);
-            }
-        }).start();
-    }
-
     public static int[] getAddedArrOfInt(int[] arr, int val) {
         int[] newArr = new int[arr.length + 1];
         System.arraycopy(arr, 0, newArr, 0, arr.length);
