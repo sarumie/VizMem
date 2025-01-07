b281d01df781074b63ef9a13155ee1f6f87bd0e9
Sandy <sandyrajalamsyah@gmail.com>
2025-01-07 16:01:11 +0700

    chore: add `playSound()`

--- src/main/java/com/imura/VizMem/Utils.java
+++ src/main/java/com/imura/VizMem/Utils.java
@@ -1,6 +1,22 @@
 package com.imura.VizMem;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public class Utils {
+    public static void playSound(String soundFile) {
+        File f = new File("src/main/resources/audio/" + soundFile);
+        try {
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(f);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void setTimeout(Runnable runnable, int delay) {
         new Thread(() -> {
             try {
